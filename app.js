41d3448473306c8cfae24411d8bd06785c10d6e4
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-10-20 20:46:40 -0400

    converts image to ascii art

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
             context.drawImage(img, 0, 0, imgWidth, imgHeight);
             let pixelData = context.getImageData(0, 0, imgWidth, imgHeight).data;
             
-            convertToGreyscale(pixelData)
+            displayArt(convertToASCII(convertToGreyscale(pixelData), imgWidth))
           };
   
           img.src = uploadedImg;
@@ -33,17 +33,36 @@ document.addEventListener('DOMContentLoaded', () => {
         reader.readAsDataURL(file);
       }
     });
-  });
 
-  function convertToGreyscale(pixelData) {
-    const greyscaleData = new Uint8ClampedArray(pixelData.length/4);
-    for (let i = 0; i < greyscaleData.length; i++) {
-        const Red = pixelData[i * 4]
-        const Green = pixelData[(i * 4) + 1]
-        const Blue = pixelData[(i * 4) + 2]
-
-        greyscaleData[i] = Math.floor(0.299 * Red + 0.587 * Green + 0.114 * Blue)
+    function convertToGreyscale(pixelData) {
+        const greyscaleData = new Uint8ClampedArray(pixelData.length/4);
+        for (let i = 0; i < greyscaleData.length; i++) {
+            const Red = pixelData[i * 4]
+            const Green = pixelData[(i * 4) + 1]
+            const Blue = pixelData[(i * 4) + 2]
+    
+            greyscaleData[i] = Math.floor(0.299 * Red + 0.587 * Green + 0.114 * Blue)
+        }
+        return greyscaleData
     }
-    console.log(greyscaleData)
-    return greyscaleData
-}
+    
+    function convertToASCII(greyscaleData, imgWidth) {
+        const asciiIntensity = [' ', '.', ',', ':', ';', '-', '~', '=', '+', '*', '#', '%', '@'];
+        const artLines = [];
+        const divider = Math.floor(255 / (asciiIntensity.length - 1));
+    
+        for (let i = 0; i < greyscaleData.length; i += imgWidth) {
+            const line = [];
+            for (let j = 0; j < imgWidth; j++) {
+                const index = Math.min(Math.floor(greyscaleData[i + j] / divider), asciiIntensity.length - 1);
+                line.push(asciiIntensity[index]);
+            }
+            artLines.push(line.join(''));
+        }
+        return artLines.join('\n');
+    }
+    
+    function displayArt(artData) {
+        asciiDisplay.innerHTML = artData
+    }
+  });
